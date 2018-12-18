35b20ef8905e4763dc053003fda4fd84004c9790
kahsumon <abdulhalim@iut-dhaka.edu>
2018-12-18 14:59:58 +0600

    d4p website uploaded phase completed

---
+++ app.R
@@ -0,0 +1,258 @@
+## app.R ##
+library(shinydashboard)
+library(shiny)
+library(DT)
+library(readxl)
+library(visNetwork)
+library(xlsx)
+library(timevis)
+
+nodes <- read_excel("./data/Nodes.xlsx")
+edges <- read_excel("./data/Edges.xlsx")
+Extremist_List <- read_excel("./data/Extremist_List.xlsx", sheet= 'Sheet1')
+Extremist <- read_excel("./data/Network_Database.xlsx", sheet= 'Extremist_Profile')
+Event <- read_excel("./data/Network_Database.xlsx", sheet= 'Violent_Event_Profile')
+Organization <- read_excel("./data/Network_Database.xlsx", sheet= 'Organizational_Profile')
+or<-data.frame(Organization)
+ex<-data.frame(Extremist[,1:40])
+ev<-data.frame(Event)
+#Data : Organizational Timeline
+templateWC <-function(stage, team1) {
+  if(stage=="Neo JMB"){
+    sprintf(
+      '<table style="background-color: #7af76a;"><tbody>
+      <tr><td colspan="2" style ="color:white;" ><b>%s</b></td></tr>
+      <tr>
+      <td>%s</td>
+      </tr>
+      <tr>
+      </tbody></table>',
+      stage, team1
+    )
+  }
+  
+  
+  else{
+    sprintf(
+      '<table style="background-color: #5ef2f2;"><tbody>
+      <tr><td colspan="2" style="color:white;" ><b>%s</b></td></tr>
+      <tr>
+      <td>%s</td>
+      </tr>
+      <tr>
+      </tbody></table>',
+      stage, team1
+    )
+  }
+} 
+
+OrgTL <- read_excel("./data/OrgTimeline.xlsx")
+data_OrgTL <-data.frame(
+  id=OrgTL$ID,
+  content =c(
+    templateWC("Neo JMB","Gulsan Holey Artisan attack"),
+    templateWC("Neo JMB","Sholakia Eid-gadh bomb blast"),
+    templateWC("Neo JMB","Japanese citizen Kunio Hoshi murder"),
+    templateWC("Neo JMB","Hindu priest Jagneshwar Roy murder"),
+    templateWC("Neo JMB","RU Prof. Rezaul Karim Siddiquee murder"),
+    templateWC("Neo JMB","Homeopathy Dr. Sanwar Hossain murder"),
+    templateWC("Neo JMB","Hindu Ashram inmate Nityaranjan Pandey murder"),
+    templateWC("Neo JMB","Pir/Sufi Mohammad Shahidullah murder"),
+    templateWC("Neo JMB","Retired Sergeant Rustom Hawlader murder"),
+    templateWC("Neo JMB","Businessman Debesh Chandra Pramanik murder"),
+    templateWC("Neo JMB","Veteran Christian businessman murder"),
+    templateWC("Neo JMB","Hindhu Priest Ananada Gopal Ganguly murder"),
+    templateWC("Neo JMB","Hindu college teacher Ripon Chakraborty murder"),
+    templateWC("Neo JMB","Hindu temple worker Shyamananda Das murder"),
+    templateWC("ABT","Blogger Asif Mohiuddin's attempted murder"),
+    templateWC("ABT","Blogger Ahmed Rajib Haider's murder"),
+    templateWC("ABT","Ashulia bank robbery"),
+    templateWC("ABT","Blogger Avijit Roy's murder"),
+    templateWC("ABT","Blogger Oyasiqur Rahman Babu's murder"),
+    templateWC("ABT","Blogger Ananta Bijoy Das's murder"),
+    templateWC("ABT","RU Prof. AKM Shafiul Islam's murder")
+  ),
+  start=OrgTL$Date
+  )
+
+
+ui <- dashboardPage(
+  skin = "purple",
+  dashboardHeader(title = "Data for Peace"),
+  
+  ## Sidebar content
+  dashboardSidebar(
+      sidebarMenu(
+      menuItem(HTML("<h5><font color ='white'><b>Extremist Profiles</b></font></h5>"), tabName = "extremist"),
+      menuItem(HTML("<h5><font color ='white'><b>Violent Event Profiles</b></font></h5>"), tabName = "incident"),
+      menuItem(HTML("<h5><font color ='white'><b>Organaizational Profiles</b></font></h5>"), tabName = "organaization"),
+      menuItem(HTML("<h5><font color ='white'><b>Network Graph</b></font></h5>"), tabName = "net"),
+      menuItem(HTML("<h5><font color ='white'><b>Organizational Timeline</b></font></h5>"), tabName = "OrgTL")
+    )
+  ),
+  #Body content
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "extremist",
+              HTML("<h1><font color ='black'><center><b>Extremist Profiles</b> </center></font></h1>"),
+              fluidRow(
+                
+                column(1),#downloadButton("downloadExt","Download")#),
+                column(10, DT::dataTableOutput('tbl_a')),
+                column(1)
+                
+              )
+      ),
+      
+      # Second tab content
+      tabItem(tabName = "incident",
+              HTML("<h1><font color ='black'><center><b>Violent Event Profiles</b> </center></font></h1>"),
+              fluidRow(
+                column(1),#downloadButton("downloadEve","Download")),
+                column(10, DT::dataTableOutput('tbl_b')),
+                column(1)
+                
+              )
+      ),
+      # Third tab content
+      tabItem(tabName = "organaization",
+              HTML("<h1><font color ='black'><center><b>Organaizational Profiles</b> </center></font></h1>"),
+              fluidRow(
+                column(1),#downloadButton("downloadOrg","Download")),
+                column(10, DT::dataTableOutput('tbl_c')),
+                column(1)
+                
+              )
+      ),
+      #Tab : Network graph
+      tabItem(tabName = "net",
+              HTML("<h1><font color ='black'><center><b>Events, Extremists & Outfits : Network Analysis</b> </center></font></h1>"),
+              
+              fluidRow(
+                visNetworkOutput("network")  
+              )
+      ),
+      #Tab : Orgnanizational Timeline
+      tabItem(tabName = "OrgTL",
+              HTML("<h1><font color ='black'><center><b>Organizational Timeline</b></center></font></h1>"),
+              
+              fluidRow(
+                column(1),
+                column(10, 
+                       timevisOutput("mytime"),
+                       actionButton("btn", "Reset")),
+                column(1)
+                      )
+      
+      
+      
+      
+      
+      
+    )
+  )
+)
+)
+
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+
+  output$tbl_c = DT::renderDT(or,extensions=c('Buttons','Responsive'),
+                                options = list(
+                                dom = 'Bfrtip',
+                                buttons = list('copy', 'print', list(
+                                  extend = 'collection',
+                                  buttons = c('csv', 'excel', 'pdf'),
+                                  text = 'Download'
+                                )),
+          
+                                searchHighlight = TRUE,
+                                pageLength=5,
+                                scrollX= TRUE)
+                              )
+                              
+                              
+  output$tbl_a = DT::renderDT(ex,
+                              extensions=c('Buttons','Responsive'),
+                              options = list(
+                                
+                                autoWidth = TRUE,
+                                columnDefs = list(list(className = 'dt-center',targets=c(1:7)),list(width ='250px',targets = list(1,7))),
+                                dom = 'Bfrtip',
+                                buttons = list('copy', 'print', list(
+                                  extend = 'collection',
+                                  buttons = c('csv', 'excel', 'pdf'),
+                                  text = 'Download'
+                                )),
+                                
+                                searchHighlight = TRUE,
+                                pageLength=5,
+                                scrollX= TRUE)
+                              )
+  output$tbl_b = DT::renderDT({datatable(ev,
+                                         extensions=c('Buttons','Responsive'),
+                                         options = list(
+                                           dom = 'Bfrtip',
+                                           buttons = list('copy', 'print', list(
+                                             extend = 'collection',
+                                             buttons = c('csv', 'excel', 'pdf'),
+                                             text = 'Download'
+                                           )),
+                                           
+                                           searchHighlight = TRUE,
+                                           pageLength=5,
+                                           scrollX= TRUE)
+                                         ) %>% formatDate('Date',method = "toDateString")})
+  output$downloadExt <- downloadHandler(
+    filename = function(){
+      paste("Extremist Profile","csv",sep = ".")
+    },
+    content = function(file) {
+      write.csv(ex,file)
+    }
+  )
+  
+  output$downloadEve <- downloadHandler(
+    filename = function(){
+      paste("Event Profile","csv",sep = ".")
+    },
+    content = function(file) {
+      write.csv(ev,file)
+    }
+  )
+  
+  output$downloadOrg <- downloadHandler(
+    filename = function(){
+      paste("Organaizational Profile","csv",sep = ".")
+    },
+    content = function(file) {
+      write.csv(or,file)
+    }
+  )
+  
+  
+  output$network <- renderVisNetwork({
+    nodes$label <- nodes$type.label
+    nodes$shadow <- TRUE
+    nodes$title <-paste0("<p><b>",nodes$media,"</b><br>",nodes$Status,"<br>",nodes$Status1,"<br>",nodes$Status2,"<br>",nodes$Status3,"<br>",nodes$Status4,"</p>")
+    nodes$borderWidth <- 2 
+    nodes$color.background <- c("slategrey", "tomato", "red")[nodes$media.type]
+    nodes$color.border <- "black"
+    nodes$color.highlight.background <- "orange"
+    nodes$color.highlight.border <- "darkred"
+    visNetwork(nodes, edges ,width="100%", height="500px")%>% visOptions(highlightNearest = list(enabled = TRUE, degree = 3),selectedBy = "type.label")%>%visInteraction(navigationButtons = TRUE,zoomView = TRUE)
+  })
+  
+  #Backend code: Extremist Organizational Timeline
+  
+  output$mytime <- renderTimevis(timevis(data_OrgTL,zoomFactor = 0.01,width = "1300px", height = "500px",fit = TRUE,showZoom = TRUE))
+  observeEvent(input$btn, {
+    addItem("mytime", list(id = "item1", content = "M", start = "2016")) %>%
+      centerItem("item1")
+  })
+  
+}
+
+shinyApp(ui, server)
